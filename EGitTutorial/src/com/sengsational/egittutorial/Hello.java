5eb7378edbad0c3fe2eac34304b234fb9a766842
sengsational <buhtig.frog4food@recursor.net>
2015-12-24 15:12:47 -0500

    second comit

--- EGitTutorial/src/com/sengsational/egittutorial/Hello.java
+++ EGitTutorial/src/com/sengsational/egittutorial/Hello.java
@@ -3,6 +3,7 @@ package com.sengsational.egittutorial;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Helooooo!");
+		System.out.println("Helooooo!");
 	}
 
 }
